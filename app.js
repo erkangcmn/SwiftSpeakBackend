22580094a587b8890b5dd04896315e6b2602508b
Erkan Göçmen <erkangocmen19@gmail.com>
2024-08-24 00:17:39 +0300

    soket hatasi duzeltildi

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const verifyToken = require("./middleware/verifytoken");
 const httpServer = http.createServer(app);
 const cors = require("cors");
 const mongoose = require("mongoose");
-const io = require("socket.io").listen(httpServer);
+const socketIO = require("socket.io"); // Import socket.io
+
+// Initialize Socket.IO
+const io = socketIO(httpServer); // Correctly instantiate socket.io
 const nsp = io.of("/chats");
 const messages = require("./services/messagesServices");
 
@@ -18,24 +21,20 @@ mongoose.connect("mongodb+srv://erkangocmn:bPa6HdYUb5UllNdT@swiftspeak.kovaksa.m
     .then(() => console.log("MongoDB: connected"))
     .catch((err) => console.log("MongoDB: error", err));
 
-
 // Routes
 const Authenticate = require("./routes/authenticate");
 const Home = require("./routes/home");
 
-
 // Middleware
-app.use(express.json({ limit: '10mb', extended: false }))
-app.use(express.urlencoded({ limit: '10mb', extended: false }))
+app.use(express.json({ limit: '10mb', extended: false }));
+app.use(express.urlencoded({ limit: '10mb', extended: false }));
 const corsOptions = {
     origin: '*',
     optionsSuccessStatus: 200,
 };
 
-
 // API anahtarını ayarla
 app.set("api_secret_key", config.api_secret_key);
-
 
 // Route kullanımı
 app.use("/", cors(corsOptions));
@@ -43,8 +42,7 @@ app.use("/", Authenticate);
 app.use("/api/", verifyToken);
 app.use("/api/", Home);
 
-
-//soketio
+// Socket.IO
 nsp.on("connection", (socket) => {
     const from = socket.handshake.query.from;
     const to = socket.handshake.query.to;
@@ -59,7 +57,7 @@ nsp.on("connection", (socket) => {
     });
 });
 
-
-
 // Sunucuyu başlat
-httpServer.listen(3031);
+httpServer.listen(3031, () => {
+    console.log('Server is listening on port 3031');
+});
